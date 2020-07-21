1c123d80f688f851be8aed3cdb7ef8d547ff351e
Sergio <sergioval16@outlook.com>
2020-07-21 17:59:33 +0200

    Ejercicio 2 Sergio

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        int[] numeros = new int[10];
+       /* int[] numeros = new int[10];
         System.out.println("introduce 10 numeros");
         for (int i = 0; i < 10; i++) {
             numeros[1] = leer.nextInt();
@@ -25,7 +25,36 @@ public class Main {
 
             }
 
+        }*/
+
+        System.out.println("Introduce cinco numeros");
+        int[] Numeros = new int[5];
+        int cont = 0, num;
+
+
+        do {
+            do {
+                System.out.println("Introduce un Numero");
+                num = leer.nextInt();
+                leer.nextLine();
+
+            } while (num < 0);
+            System.out.println("Introduce un Numero");
+            num = leer.nextInt();
+            leer.nextLine();
+            if (num > 0) {
+                Numeros[cont] = num;
+                cont++;
+            }
+
+
+        } while (cont < 5);
+        for (int i = 4; i <= 0; i++) {
+            System.out.println(Numeros[i] + ",");
+
+
         }
+
     }
 
 
