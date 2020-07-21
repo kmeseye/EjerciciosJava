b46955182bf10494b66452d20a97c7fa8ef21e2f
Sergio <sergioval16@outlook.com>
2020-07-21 17:17:16 +0200

    Ejercicio Sergio

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner leer = new Scanner(System.in);
+        int[] numeros = new int[10];
+        System.out.println("introduce 10 numeros");
+        for (int i = 0; i < 10; i++) {
+            numeros[1] = leer.nextInt();
+            leer.nextLine();
+
+        }
+        System.out.println("Introduce el numero a buscar");
+        int numero = leer.nextInt();
+        leer.nextLine();
+        int contador = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > numero) {
+                contador++;
+
+            }
+
+        }
     }
+
+
 }
